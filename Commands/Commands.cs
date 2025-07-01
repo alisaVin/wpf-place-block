feb67967a42bd33dc95c2a66436629e4ce14634b
Alisa Vinogradova <avinogradova@kesslersolutions.de>
2025-07-01 12:48:10 +0200

    initial commit

---
+++ Commands/Commands.cs
@@ -0,0 +1,13 @@
+﻿using Teigha.Runtime;
+
+namespace Commands
+{
+    public class ARESCommands
+    {
+        [CommandMethod("BLOCKPL")]
+        public static void Demo()
+        {
+
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+#if ARES2021_x64
+[assembly: AssemblyTitle("place_block_wpf for ARES (2021 64bit)")]
+[assembly: AssemblyDescription("WPF Plugin to plasce ACAD blocks in destination DWG on the sertain coordinates from Excel table for ARES 2021-22 64Bit")]
+#endif
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Keßler Real Estate Solutions GmbH")]
+[assembly: AssemblyProduct("place_block_wpf (ares)")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("35fc4dec-fa58-41dc-af6e-7ab9e79aa63d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
